b9c430478c5653fb76fec5640b52df364a14cf4f
Lucas Moreno <devlmoreno007@gmail.com>
2022-04-10 22:22:12 -0300

    Added route validation and method validation start (get)

--- app/Http/Router.php
+++ app/Http/Router.php
@@ -2,6 +2,10 @@
 
 namespace App\Http;
 
+use \Closure;
+use \Exception;
+use JetBrains\PhpStorm\ExpectedValues;
+
 class Router {
 
     /**
@@ -49,6 +53,98 @@ class Router {
 
         // DEFINE O PREFIXO
         $this->prefix = $parseUrl['path'] ?? '';
+    }
+
+    /**
+     * Método responsável por adicionar uma rota na classe
+     * @param string $method
+     * @param string $route
+     * @param array  $params
+     */
+    private function addRoute(string $method, string $route, array $params = [])
+    {
+        // VALIDAÇÃO DOS PARÂMETROS
+        foreach ($params as $key => $value) {
+            if ($value instanceof Closure) {
+                $params['controller'] = $value;
+                unset($params[$key]);
+            }
+        }
+
+        // PADRÃO PARA VALIDAR A URL - REGEX
+        $patternRoute = '/^' . str_replace('/', '\/', $route) . '$/';
+
+        // ADICIONA A ROTA DENTRO DA CLASSE
+        $this->routes[$patternRoute][$method] = $params;
+    }
+
+    /**
+     * Método responsável por definir uma rota de GET
+     * @param string $route
+     * @param array  $params
+     */
+    public function get(string $route, array $params = [])
+    {
+        return $this->addRoute('GET', $route, $params);
+    }
+
+    /**
+     * Método responsável por retornar a URI desconsiderando o prefixo
+     * @return string
+     */
+    private function getUri()
+    {
+        // URI DA REQUEST
+        $uri = $this->request->getUri();
+
+        // FATIA A URI COM O PREFIXO
+        $xUri = strlen($this->prefix) ? explode($this->prefix, $uri) : [$uri];
+
+        // RETORNA A URI SEM PREFIXO
+        return end($xUri);
+    }
+
+    /**
+     * Método responsável por retornar os dados da rota atual
+     * @return array
+     */
+    private function getRoute()
+    {
+        // URI
+        $uri = $this->getUri();
+
+        // MÉTHOD
+        $httpMethod = $this->request->getHttpMethod();
+
+        // VALIDA AS ROTAS
+        foreach ($this->routes as $patternRoute => $methods) {
+            // VERIFICA SE A URI BATE COM O PADRÃO
+            if (preg_match($patternRoute, $uri)) {
+                // VERIFICA O MÉTODO
+                if ($methods[$httpMethod]) {
+                    // RETORNO DOS PARÂMETROS DA ROTA
+                    return $methods[$httpMethod];
+                }
+
+                throw new Exception("Método não permitido", 405);
+            }
+        }
+
+        throw new Exception("URL não encontrada", 404);
+    }
+
+    /**
+     * Método responsável por executar a rota atual
+     * @return Response
+     */
+    public function run()
+    {
+        try {
+            // OBTEM A ROTA ATUAL
+            $route = $this->getRoute();
+        } catch (Exception $e) {
+            return new Response($e->getCode(), $e->getMessage());
+        }
     }
 
 }
--- index.php
+++ index.php
@@ -3,13 +3,20 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use \App\Http\Router;
+use \App\Http\Response;
 use \App\Controller\Pages\Home;
 
 define('URL', 'http://localhost:8000');
 
-$obj = new Router(URL);
-echo "<pre>";
-print_r($obj);
-exit;
+$objRouter = new Router(URL);
 
-echo Home::getHome();
+// ROTA HOME
+$objRouter->get('/', [
+    function() { // função anônima
+        return new Response(200, Home::getHome());
+    }
+]);
+
+// IMPRIME O RESPONSE DA ROTA
+$objRouter->run()
+            ->sendResponse();
