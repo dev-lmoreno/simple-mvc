4837d33eb8cb1ce47a19b4b24e50fa92d213afe2
Lucas Moreno <devlmoreno007@gmail.com>
2022-04-10 21:41:03 -0300

    Added initial part of Router class

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use \App\Http\Router;
 use \App\Controller\Pages\Home;
+
+define('URL', 'http://localhost:8000');
+
+$obj = new Router(URL);
+echo "<pre>";
+print_r($obj);
+exit;
 
 echo Home::getHome();
